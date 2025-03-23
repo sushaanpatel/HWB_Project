ecc139f09583a1809e22d17082d7e217ac2ce101
Sushaan Patel <sushaanpatel@gmail.com>
2025-03-23 12:23:10 -0400

    integrating ocr and cv

--- ocr.py
+++ ocr.py
@@ -1,20 +1,46 @@
 import easyocr
+from spellchecker import SpellChecker
 from speak import say
 
-def ocr(image_path):
-    reader = easyocr.Reader(['en'])
-    print("Reading Text")
-    result = reader.readtext(image_path)
-    return result
+reader = easyocr.Reader(['en'])
+spell = SpellChecker()
 
-if __name__ == '__main__':
-    data = ocr("data/frame1.jpg")
+
+def spell_correct(text: str):
+    temp = text.split(" ")
+    out = ""
+    for i in range(len(temp)):
+        corrected = spell.correction(temp[i])
+        valid_text = (corrected if corrected is not None else temp[i])
+        if i == len(temp) - 1:
+            out += valid_text
+        else:
+            out += valid_text + " "
+    return out
+
+def ocr(image_path: str):
+    print("Reading Text")
+    data = reader.readtext(image_path)
+
     string = ""
     for (bbox, text, prob) in data:
-        string += text + " "
+        print(text, prob)
+        if prob >= 0.5:
+            temp = spell_correct(text)
+            string += (temp if temp is not None else text) + " "
 
-    # print(string)
+    print(string)
     say(string)
 
+if __name__ == '__main__':
+    # data = ocr("data/frame3.jpg")
+    # string = ""
+    # for (bbox, text, prob) in data:
+    #     print(text, prob)
+    #     if prob >= 0.5:
+    #         temp = spell_correct(text)
+    #         string += (temp if temp is not None else text) + " "
 
-
+    # print(string)
+    # say(string)
+    pass
